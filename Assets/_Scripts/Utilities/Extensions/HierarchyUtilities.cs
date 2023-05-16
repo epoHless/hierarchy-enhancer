873e7c652301a9536e6cace7af1a6501f8abe059
Kevin Salimbeni <73953277+epoHless@users.noreply.github.com>
2023-05-16 16:16:29 +0200

    First Init

---
+++ Assets/_Scripts/Utilities/Extensions/HierarchyUtilities.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public static class HierarchyUtilities
+{
+    /// <summary>
+    /// Create a texture with a given color
+    /// </summary>
+    /// <param name="_width"></param>
+    /// <param name="_height"></param>
+    /// <param name="_col"></param>
+    /// <returns></returns>
+    public static Texture2D DrawCube(int _width, int _height, Color _col)
+    {
+        Color[] pix = new Color[_width*_height];
+ 
+        for(int i = 0; i < pix.Length; i++)
+            pix[i] = _col;
+ 
+        Texture2D result = new Texture2D(_width, _height);
+        result.SetPixels(pix);
+        result.Apply();
+ 
+        return result;
+    }
+    
+    public static Texture2D DrawCircle(this Texture2D _tex, Color _color, int _x, int y, int _radius = 3)
+    {
+        float rSquared = _radius * _radius;
+
+        for (int u = _x - _radius; u < _x + _radius + 1; u++)
+        for (int v = y - _radius; v < y + _radius + 1; v++)
+            if ((_x - u) * (_x - u) + (y - v) * (y - v) < rSquared)
+                _tex.SetPixel(u, v, _color);
+
+        return _tex;
+    }
+    
+    /// <summary>
+    /// Set the parameter color to a brighter or darker one
+    /// </summary>
+    /// <param name="color">the color to modify</param>
+    /// <param name="correctionFactor">|1 = default | <1 = darker | >1 = Lighter |</param>
+    /// <returns></returns>
+    public static Color ChangeColorBrightness(Color color, float correctionFactor)
+    {
+        return new Color(color.r * correctionFactor, color.g * correctionFactor, color.b * correctionFactor, 1f);
+    }
+}
---
+++ Assets/_Scripts/Utilities/HierarchyEnhancher/Editor/HierarchyWindowGameObjectLabel.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+#if UNITY_EDITOR
+
+[InitializeOnLoad]
+public static class HierarchyWindowGameObjectLabel
+{
+    public static string LabelsDirectory { get; private set; }
+    
+    public static List<HierarchyLabelPreset> Presets = new List<HierarchyLabelPreset>();
+
+    private static readonly Color SelectedColor = new Color(44f / 255f, 93f / 255f, 135f / 255f, 1f);
+    private static readonly Color UnselectedColor = new Color(56f / 255f, 56f / 255f, 56f / 255f);
+
+    static HierarchyWindowGameObjectLabel()
+    {
+        EditorApplication.hierarchyWindowItemOnGUI += HandleHierarchyWindowItemOnGUI;
+        AssemblyReloadEvents.afterAssemblyReload += FetchLabels;
+    }
+
+    public static void FetchLabels()
+    {
+        if (!Directory.Exists($"{Application.dataPath}/HierarchyLabels"))
+            AssetDatabase.CreateFolder("Assets", "HierarchyLabels");
+        
+        LabelsDirectory = "Assets/HierarchyLabels/";
+
+        var assets = AssetDatabase.FindAssets("", new[] { LabelsDirectory });
+
+        Presets = new List<HierarchyLabelPreset>();
+        
+        foreach (var asset in assets)
+        {
+            var path = AssetDatabase.GUIDToAssetPath(asset);
+            var item = AssetDatabase.LoadAssetAtPath(path, typeof(HierarchyLabelPreset)) as HierarchyLabelPreset;
+
+            if (item)
+            {
+                AddPreset(item);
+            }
+        }
+    }
+
+    static void HandleHierarchyWindowItemOnGUI(int _instanceID, Rect _selectionRect)
+    {
+        var content = EditorGUIUtility.ObjectContent(EditorUtility.InstanceIDToObject(_instanceID), null);
+        var gameobject = EditorUtility.InstanceIDToObject(_instanceID) as GameObject;
+        
+        if(gameobject) RenderLines(_selectionRect, gameobject, Color.gray);
+
+        foreach (var preset in Presets)
+        {
+            if (content.text.StartsWith(preset.identifier))
+            {
+                string text = content.text.Remove(0,preset.identifier.Length);
+
+                if(gameobject) RenderLines(_selectionRect, gameobject, preset.textColor);
+
+                GUI.Label(_selectionRect, text, SetStylePreset(preset, _instanceID));
+
+                if (content.image != null && preset.icon)
+                {
+                    GUI.DrawTexture(new Rect(_selectionRect.xMax - 20, _selectionRect.yMin, 20, 16), 
+                        HierarchyUtilities.DrawCube(1,1,
+                            EditorUtility.InstanceIDToObject(_instanceID) == Selection.activeObject ? SelectedColor : UnselectedColor));
+                    
+                    GUI.DrawTexture(new Rect(_selectionRect.xMax - 16, _selectionRect.yMin, 15, 15), preset.icon);
+                }
+            }
+        }
+    }
+
+    private static void RenderLines(Rect _selectionRect, GameObject _gameObject, Color _color)
+    {
+        if(_gameObject.transform.childCount > 0)
+        {
+            GUI.DrawTexture(new Rect(_selectionRect.xMin - 24.5f, _selectionRect.yMin + 5, 7, 7),
+                HierarchyUtilities.DrawCube(1, 1, _color));
+        }
+
+        if (_gameObject.transform.parent != null)
+        {
+            GUI.DrawTexture(new Rect(_selectionRect.xMin - 35, _selectionRect.yMin + 7.5f, 15, 1.5f),
+                HierarchyUtilities.DrawCube(1, 1, _color));
+
+            GUI.DrawTexture(new Rect(_selectionRect.xMin - 36.5f, _selectionRect.yMin, 2, 17),
+                HierarchyUtilities.DrawCube(1, 1, _color));
+        }
+    }
+
+    private static bool IsGameObjectEnabled(int _instanceID)
+    {
+        return EditorUtility.GetObjectEnabled(EditorUtility.InstanceIDToObject(_instanceID)) != 0;
+    }
+
+    private static GUIStyle SetStylePreset(HierarchyLabelPreset _preset, int _instanceID)
+    {
+        GUIStyle style = new GUIStyle();
+
+        var normalStyleState = IsGameObjectEnabled(_instanceID) ? new GUIStyleState
+        {
+            background = EditorUtility.InstanceIDToObject(_instanceID) != Selection.activeObject ? HierarchyUtilities.DrawCube(1, 1, _preset.backgroundColor) : HierarchyUtilities.DrawCube(1, 1, SelectedColor),
+            textColor = _preset.textColor
+        } : new GUIStyleState
+        {
+            background = EditorUtility.InstanceIDToObject(_instanceID) != Selection.activeObject ? HierarchyUtilities.DrawCube(1, 1, _preset.inactiveBackgroundColor) : HierarchyUtilities.DrawCube(1, 1, SelectedColor),
+            textColor = _preset.useCustomInactiveColors ? _preset.inactiveTextColor : HierarchyUtilities.ChangeColorBrightness(_preset.textColor, 0.45f)
+        };
+
+        style.normal = normalStyleState;
+        
+        style.fontStyle = _preset.fontStyle;
+        style.alignment = _preset.alignment;
+        
+        return style;
+    }
+
+    public static void AddPreset(HierarchyLabelPreset _preset)
+    {
+        if (!Presets.Contains(_preset))
+        {
+            Presets.Add(_preset);
+        }
+        
+        EditorApplication.RepaintHierarchyWindow();
+    }
+    
+    public static void RemovePreset(HierarchyLabelPreset _preset)
+    {
+        if (Presets.Contains(_preset))
+        {
+            Presets.Remove(_preset);
+        }
+        
+        EditorApplication.RepaintHierarchyWindow();
+    }
+}
+
+#endif
