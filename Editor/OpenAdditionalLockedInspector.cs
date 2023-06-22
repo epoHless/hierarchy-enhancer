f0cbd46b9a275820ff28a2b4c31bcb6b2b7bf888
Kevin Salimbeni <73953277+epoHless@users.noreply.github.com>
2023-06-22 14:40:54 +0200

    Moved repository

---
+++ Editor/OpenAdditionalLockedInspector.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace HierarchyEnhancer
+{
+    public static class OpenAdditionalLockedInpsector
+    {
+        public static void DisplayLockedInspector()
+        {
+            // Create new inspector window
+            var inspectorWindow = ScriptableObject.CreateInstance(GetInspectorWindowType()) as EditorWindow;
+            inspectorWindow.Show();
+
+            // Lock new inspector window
+            LockInspector(GetInspectorWindowType());
+        }
+
+        private static Type GetInspectorWindowType()
+        {
+            return typeof(Editor).Assembly.GetType("UnityEditor.InspectorWindow");
+        }
+
+        private static void LockInspector(Type _obj)
+        {
+            var fields = _obj.GetFields(BindingFlags.Instance | BindingFlags.Public);
+
+            foreach (var field in fields)
+            {
+                if (field.Name == "isLocked")
+                    field.SetValue(_obj, true);
+            }
+        }
+    }
+}
