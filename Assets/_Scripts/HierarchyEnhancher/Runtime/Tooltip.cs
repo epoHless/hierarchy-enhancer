54ee164f2634f676103f99387d795967e634353f
Kevin Salimbeni <73953277+epoHless@users.noreply.github.com>
2023-06-22 14:33:37 +0200

    Added missing namespaces, fixed folders and formatting

---
+++ Assets/_Scripts/HierarchyEnhancher/Runtime/Tooltip.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace HierarchyEnhancer
+{
+    [System.Serializable]
+    public class Tooltip
+    {
+        public string tooltip;
+        public Texture icon;
+        [HideInInspector] public string info;
+    }
+}
